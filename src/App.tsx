633926b73e58c73d04bb18c4b7368597689e7f9f
Nils Björklin <nils.bjorklin@gmail.com>
2024-05-03 20:06:48 +0200

    Added funtionality to display, add and delete exercises

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,55 @@
-import type { Component } from 'solid-js';
+import { createSignal, type Component, For, createEffect } from 'solid-js';
+import { createStore } from "solid-js/store";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+type Exercise = {
+    name: string
+    weight: string
+}
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+    const [exercise, setExercise] = createSignal<Exercise[]>([])
+    const [fields, setFields] = createStore<Record<string, any>>()
+
+    function addExercise(e: Event) {
+        console.log(fields);
+
+        e.preventDefault()
+        setExercise((prev) => {
+            return [...prev, { name: fields.name, weight: fields.weight }]
+        })
+        setFields(Object.keys(fields), undefined);
+    }
+
+    function removeExercise(index: number) {
+
+        setExercise((prev) => {
+            return [...prev.filter((exercise, i) => i !== index)]
+        })
+        setFields(Object.keys(fields), undefined);
+    }
+
+    return (
+        <div>
+            <div>
+                <For each={exercise()}>{(exercise, index) =>
+                    <p>{exercise.name}: {exercise.name}kg
+                        <button
+                            type='submit'
+                            class='bg-red-600 rounded text-white py-1 px-4 hover:bg-white hover:text-gray-700'
+                            onclick={() => removeExercise(index())}>
+                                ta bort
+                        </button></p>
+                }</For>
+            </div>
+            <div class='bg-gray-300'>
+                <form onSubmit={addExercise}>
+                    <input class='bg-inherit' value={fields.name ?? ''} placeholder='exercise name' onInput={(e) => setFields('name', e.target.value)} />
+                    <input class='bg-inherit' value={fields.weight ?? ''} placeholder='weight' onInput={(e) => setFields('weight', e.target.value)} />
+                    <button type='submit' class='bg-gray-700 rounded text-white py-1 px-4 hover:bg-white hover:text-gray-700'>submit</button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default App;
